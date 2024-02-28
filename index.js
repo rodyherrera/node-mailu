2cc262250a01dd9d1520c593d30844b7a915001e
rodyherrera <contact@rodyherrera.com>
2024-02-28 18:03:27 -0300

    feat: added relay-related routes.

--- index.js
+++ index.js
@@ -302,6 +302,47 @@ class MailuClient{
     getRelays(){
         return this.useAxios('get', '/relay');
     };
+
+    /**
+     * Creates a new relay.
+     *
+     * @param {Object} body - The body of the request containing the relay data.
+     * @returns {Promise} A Promise that resolves with the response from the server.
+    */
+    createRelay(body){
+        return this.useAxios('post', `/relay`, body);
+    };
+
+    /**
+     * Updates an existing relay.
+     *
+     * @param {string} name - The unique name of the relay to update.
+     * @param {Object} body - The updated relay data.
+     * @returns {Promise} A Promise that resolves with the response from the server.
+    */
+    updateRelay(name, body){
+        return this.useAxios('patch', `/relay/${name}`, body);
+    };
+
+    /**
+     * Retrieves a specific relay based on its name.
+     *
+     * @param {string} name - The unique name of the relay.
+     * @returns {Promise} A Promise that resolves with the response from the server, containing the relay data.
+    */
+    getRelay(name){
+        return this.useAxios('get', `/relay/${name}`);
+    };
+
+    /**
+     * Deletes an existing relay.
+     *
+     * @param {string} name - The unique name of the relay to delete.
+     * @returns {Promise} A Promise that resolves with the response from the server.
+    */
+    deleteRelay(name){
+        return this.useAxios('delete', `/relay/${name}`);
+    };
 };
 
 module.exports = MailuClient;
